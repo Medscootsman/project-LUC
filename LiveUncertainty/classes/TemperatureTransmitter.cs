a03b3cf11374ceef66885da786845fc33b3d722f
Medscootsman <murraylyne2@Gmail.com>
2018-01-28 00:05:13 +0000

    Added some classes to be worked on. Expect more soon.

---
+++ LiveUncertainty/classes/TemperatureTransmitter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class TemperatureTransmitter
+{
+	public TemperatureTransmitter()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
