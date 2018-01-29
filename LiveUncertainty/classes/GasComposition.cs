dfa1f765653f9371bc5ef2cdab4c5f253432d6f3
Medscootsman <murraylyne2@Gmail.com>
2018-01-29 21:39:28 +0000

    fixed minor mistake

--- LiveUncertainty/classes/GasComposition.cs
+++ LiveUncertainty/classes/GasComposition.cs
@@ -4,9 +4,10 @@
 /// gas mol compositions class.
 /// </summary>
 public class GasComposition
-    public double methane;
+    
 {
-	public GasComposition()
+    public double methane;
+    public GasComposition()
 	{
 		//
 		// TODO: Add constructor logic here
