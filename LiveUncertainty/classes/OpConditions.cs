7e3f4ac84d10633bdd592c8bf1fc09442b144c2c
Medscootsman <murraylyne2@Gmail.com>
2018-02-04 22:39:14 +0000

    code

--- LiveUncertainty/classes/OpConditions.cs
+++ LiveUncertainty/classes/OpConditions.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace LiveUncertainty.classes
 {
-    class OperatingCOnditions
+    class OperatingConditions
     {
+        public double opPressure;
+        public double opTemperature;
+        public double opDensity;
+        public double baseDensity;
+        public double refTemperature;
+        public double refPressure;
+        public bool PTZ_calc;
+        public double maxMassFlow;
+        public double maxGOVFlow;
+        public Int16 measurementType;
+        public OperatingConditions()
+        {
+
+        }
     }
 }
