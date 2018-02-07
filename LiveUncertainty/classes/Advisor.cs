0f384650c0007e2fdbde865b63d0fd603b9abb4c
Medscootsman <murraylyne2@Gmail.com>
2018-02-07 22:30:47 +0000

    classifier: will be worked on once everything else is working

---
+++ LiveUncertainty/classes/Advisor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveUncertainty
+{
+    class Advisor
+    {
+        public double uncertaintyTarget;
+        public double reductionPotential;
+        public bool feasible;
+
+        public Advisor()
+        {
+
+        }
+
+        public void Analyse()
+        {
+
+        }
+
+        public void extractFeatures()
+        {
+            
+        }
+
+        public Double Target
+        {
+            get
+            {
+                return uncertaintyTarget;
+            }
+            set
+            {
+                uncertaintyTarget = value;
+            }
+
+        }
+
+        public Double PotentialReduction
+        {
+            get
+            {
+                return reductionPotential;
+            }
+
+            set
+            {
+                reductionPotential = value;
+            }
+        }
+
+        public bool Feasible
+        {
+            get
+            {
+                return feasible;
+            }
+
+            set
+            {
+                feasible = value;
+            }
+        }
+
+    }
+}
