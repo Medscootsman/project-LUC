b448bda7c150cc128bcf812de0d6584affbb89d1
Medscootsman <murraylyne2@Gmail.com>
2018-02-07 22:23:20 +0000

    flowcomp structure

--- LiveUncertainty/classes/FlowComputer.cs
+++ LiveUncertainty/classes/FlowComputer.cs
@@ -8,5 +8,64 @@ namespace LiveUncertainty.classes
 {
     class FlowComputer
     {
+        public string tagNo;
+        public string manufacturer;
+        public string type;
+        public double calUncertainty;
+        public FlowComputer()
+        {
+            //constructor logic tbc
+        }
+
+        public string TagNo
+        {
+            get
+            {
+                return tagNo;
+            }
+            set
+            {
+                tagNo = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return manufacturer;
+            }
+
+            set
+            {
+                manufacturer = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            
+            set
+            {
+                type = value;
+            }
+        }
+
+        public double Uncertainty
+        {
+            get
+            {
+                return calUncertainty;
+            }
+
+            set
+            {
+                calUncertainty = value;
+            }
+        }
     }
 }
