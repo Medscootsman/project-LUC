682bcc3ee79e99846ad9ab65ec25eb8c947ec909
Murray Lyne <murraylyne2@Gmail.com>
2018-02-11 20:03:01 +0000

    outerdiameter and meterborewallthickness complete.

--- LiveUncertainty/classes/USM.cs
+++ LiveUncertainty/classes/USM.cs
@@ -9,7 +9,7 @@ namespace LiveUncertainty.classes
     {
         protected Int64 manufacturer_id;
         protected Int64 model_id;
-        public Int16 paths;
+        public Int16 pathsTotal;
         public Int16 signal_output;
         public double calFrequency; //calibration frequency.
         public double nomDiameter; //Nominal Diameter.
@@ -22,11 +22,20 @@ namespace LiveUncertainty.classes
         public double calLabUncertainty; //calibration lab uncertainty
         public double calTemperature; //Calibration Temperature
         public double calPressure; //Calibration Pressure;
-        public bool metrologyData; //Always will be true (for now).
+        public bool metrologyData = true; //Always will be true (for now).
         public double transducerDistance;
         public double pathLengthTolerance;
         public double fluidPathTolerance;
         public double meterDiameterTolerance;
+        public const double thermalExpansion = 0.000011;
+
+        public List<Path> paths;
+        public List<Double> pathLengths;
+        public List<Double> pathChords;
+        public List<Double> pathWeightingFactors;
+        public List<Double> pathAngles;
+        
+
 
         public UltraSonicMeter()
         {
@@ -52,12 +61,12 @@ namespace LiveUncertainty.classes
         {
             get
             {
-                return paths;
+                return pathsTotal;
             }
 
             set
             {
-                paths = value;
+                pathsTotal = value;
             }
         }
 
@@ -276,6 +285,61 @@ namespace LiveUncertainty.classes
                 meterDiameterTolerance = value;
             }
         }
+
+        //meter tube bore is calculated by dividing the internal diameter by 1000. (Mathcad reference: dDry)
+        public double calculateMeterTubeBore()
+        {
+            double metertubebore = this.internalDiameter / 1000;
+            return metertubebore;
+        }
+
+        //NOTE: outerdiameter and meterwallthickness can be 0, but 1 has to be filled in at one time.
+
+        //Mathcad reference (tw)
+        public double calculateMeterWallThickness()
+        {
+
+            double tw = 0; //metertubewallthickness
+            if(wallThickness != 0)
+            {
+                tw = wallThickness / 1000;
+            }
+            else
+            {
+                //tw is outerDiameter multiplied by 10 to the power of 3 taken away by the metertubebore divided by 2.
+
+                tw = (outerDiameter * Math.Pow(10, 3)) - this.calculateMeterTubeBore() / 2;
+            }
+
+            return tw;
+        }
+
+        public double calculateOuterDiameter()
+        {
+            double dOut = 0; //true outer diameter.
+            if(outerDiameter != 0)
+            {
+                //this is bore plus 2 multiplied by meterwallthickness or tw
+                dOut = calculateMeterTubeBore() + 2 * calculateMeterWallThickness();
+            }
+            else
+            {
+                dOut = outerDiameter / 1000;
+            }
+
+            return dOut;
+        }
+
+        
+        public void addPath(Path path)
+        {
+            paths.Add(path);
+        }
+
+        
+
+
+
 
     }
 }
