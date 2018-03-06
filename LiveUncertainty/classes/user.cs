1fb43187b5531e320ac89ada7da0725e7c9a3a33
Medscootsman <murraylyne2@Gmail.com>
2018-03-06 22:47:51 +0000

    Settings window
    added a settings window.

---
+++ LiveUncertainty/classes/user.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveUncertainty.classes
+{
+    public class User
+    {
+        String name;
+        String initials;
+        String job_Title;
+
+        public User()
+        {
+            name = "John";
+            initials = "JD";
+            job_Title = "Measurement Engineer";
+
+        }
+
+        string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        String Initials
+        {
+            get => name;
+            set => name = value;
+        }
+
+        String Job_Title
+        {
+            get => name;
+            set => name = value;
+        }
+    }
+}
